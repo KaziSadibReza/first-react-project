afb5b57e0cf65947c5167ecacf15448e9adb60be
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-05-04 22:00:44 +0600

    add component to our app

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <div className="card-header">Header</div>
+        <div className="card-body">Body</div>
+        <div className="card-footer">Footer</div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
